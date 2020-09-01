625eead39ffa4a5b33950d6cff62c939f706601b
Kir <suvkurst@mail.ru>
2020-09-01 12:22:11 +0300

    //Добавил домашнее задание к первому уроку

---
+++ 1l_SuvorovK.playground/Sources/2task.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+
+
+let a1 : Double = 3
+let b1 : Double = 4
+let c1 : Double  = sqrt(a1*a1 + b1*b1) //Высчитываем гипотенузу.
+let perimeter : Double = a1+b1+c1 //Высчитываем периметр.
+let area : Double = a1*b1 / 2
